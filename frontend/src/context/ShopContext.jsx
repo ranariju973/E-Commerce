10e881c250f9d3720e41156ffc5c5a7d377f5631
ranariju973 <ranariju973@gmail.com>
2026-03-28 01:43:32 +0530

    Initial commit

---
+++ frontend/src/context/ShopContext.jsx
@@ -0,0 +1,22 @@
+import { ShopContext } from './ShopContext.js'
+import { products } from '../assets/assets'
+
+const ShopContextProvider = (props) => {
+
+    const currency = '₹'
+    const delivery_fee = 10
+
+    const value = {
+        products,
+        currency,
+        delivery_fee
+    }
+
+    return (
+        <ShopContext.Provider value={value}>
+            {props.children}
+        </ShopContext.Provider>
+    )
+}
+
+export default ShopContextProvider
